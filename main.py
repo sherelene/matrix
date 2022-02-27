03347e9c7148051a5f057483966f99d3cbc0c548
sherelene <71803296+sherelene@users.noreply.github.com>
2022-02-27 04:44:19 -0800

    fixed n and m. m should be n +  1, not n - 1

--- main.py
+++ main.py
@@ -7,18 +7,12 @@ def naive_gaussian(coeffs, consts):
     forward_elimination(coeffs, consts)
 
 
-
 def forward_elimination(coeffs, consts):
     for k in range(col - 1):
         for i in range(k+1, rows):
             coeffs[i, :] = (coeffs[i, k]/coeffs[k, k]) * coeffs[k,:] - coeffs[i, :]
     print(coeffs)
     backward_elimination(coeffs, consts)
-
-
-
-    #print("new coefficients", coefficient)
-    #print("new constants", consts)
 
 
 def backward_elimination(coeffs, consts):
@@ -34,7 +28,7 @@ with open("practice_input.txt") as filename:
 
     # reads the first line in the file that has the size of the matrix
     n = int(filename.readline())
-    m = n - 1
+    m = n + 1
     # reads the rest of the lines within the file
     lines = filename.readlines()
 
@@ -43,23 +37,24 @@ with open("practice_input.txt") as filename:
     for line in lines:
         content.append(line.split(" "))
 
+
     # cleans up data from file even more by getting rid of any "\n" found in the arrays
     content = [[s.rstrip('\n') for s in nested if not s.isspace()] for nested in content]
     content = [nested for nested in content if nested]
 
     # declare and initialize array that we will start using as coefficients
-    coefficient = [[0 for i in range(m)] for j in range(n)]
-    for i in range(n):
-        for j in range(m):
+    coefficient = [[0 for i in range(n)] for j in range(m)]
+    for i in range(m):
+        for j in range(n):
             coefficient[i][j] = float(content[i][j])
 
 
     # declare and initialize array that we will start using as constants
     constant_array = [0 for i in range(n)]
-    for j in range(m):
+    for j in range(n):
         #constant_array[j] = float(content[n][j])
         last_column = coefficient[j]
-        last_column.append(float(content[m][j]))
+        last_column.append(float(content[n][j]))
     # make constant array into a vector
     constant = np.array(constant_array)
     matrix = coefficient[:-1]
